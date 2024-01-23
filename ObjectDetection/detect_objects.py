ed6643b276e9bedf1f8a16093a3e17a480b6ff1f
rebmizrahi <becca.mizrahi@gmail.com>
2024-01-23 18:14:13 -0500

    Combined differencing + alignment code into one file. Added object detection after pixel differencing.

--- ObjectDetection/detect_objects.py
+++ ObjectDetection/detect_objects.py
@@ -10,6 +10,9 @@ from scipy.ndimage import shift, median_filter
 from scipy.ndimage import gaussian_filter
 from photutils.utils import make_random_cmap
 from photutils import detect_threshold, detect_sources
+from skimage.transform import resize
+import random
+
 """
 Pre-processing algorithm that applies peak finding, background subtraction
 with a masked gaussian blur filter + darkening, and alignment based on peaks.
@@ -19,8 +22,10 @@ photometry cleaning algorithm.
 """
 
 INPUT_PATH = 'mission_images/'
-OUTPUT_PATH = 'outgoing/'
-
+ALIGNED_PATH = 'aligned/'
+GAUSSIANBLUR_PATH = 'gaussian_blur/'
+DIFFERENCED_PATH = 'differenced/'
+FLAGGED_PATH = 'flagged/'
 
 def align(sources_image1, sources_image2, image2_filename):
 
@@ -36,21 +41,19 @@ def align(sources_image1, sources_image2, image2_filename):
     shift_y = ref_star_coords_image1[1] - ref_star_coords_image2[1]
 
     # Shift one image relative to the other using interpolation
-    with fits.open(INPUT_PATH + 'bg_processed_' + image2_filename, mode='update') as hdul:
-        print(f"dimensions of image: {hdul[0].data.shape}")
+    with fits.open(GAUSSIANBLUR_PATH + image2_filename, mode='update') as hdul:
         shifted_data = shift(hdul[0].data, (shift_y, shift_x), mode='nearest')
         hdul[0].data = shifted_data
         
         hdu = fits.PrimaryHDU(shifted_data, header=hdul[0].header)
         hdul_out = fits.HDUList([hdu])
-        hdul_out.writeto(OUTPUT_PATH + 'aligned_' + image2_filename, overwrite=True)
-        print(f"dimensions of shifted image: {hdul_out[0].data.shape}")
-    print(f"Aligning images with a shift of ({shift_x}, {shift_y}) pixels.")
+        hdul_out.writeto(ALIGNED_PATH + image2_filename, overwrite=True)
+    #print(f"Aligning images with a shift of ({shift_x}, {shift_y}) pixels as {ALIGNED_PATH + image2_filename}.")
 
 
-def detect_objects(filename):
+def detect_objects(filepath):
     # Open the FITS file
-    with fits.open(INPUT_PATH + filename) as hdul:
+    with fits.open(filepath) as hdul:
         data = hdul[0].data  # Accessing the data from the primary HDU
 
     # Calculate background statistics
@@ -89,22 +92,46 @@ def subtract_background(filename, sources):
         inverted_mask = 1 - mask
         blurred_image = cv2.GaussianBlur(image.astype(np.float32), (15, 15), 4)
         
-        # Darkening factor for the blurred regions
-        darkening_factor = 0.5  # Adjust this value as needed
-
-        # Darken the blurred areas before blending with the original image
-        darkened_blurred = blurred_image * darkening_factor
-        
         # Combine the original image with the darkened blurred image using the masks
-        final_image = image * inverted_mask + darkened_blurred * mask
+        final_image = image * inverted_mask + blurred_image * mask
 
         # Save the background-subtracted and darkened image to a new FITS file
-        output_filename = INPUT_PATH + 'bg_processed_' + filename
+        output_filename = GAUSSIANBLUR_PATH + filename
         hdu = fits.PrimaryHDU(final_image, header=hdul[0].header)
         hdul_out = fits.HDUList([hdu])
         hdul_out.writeto(output_filename, overwrite=True)
 
-        print(f"Background-subtracted and darkened image saved as {output_filename}.")
+"""
+Perform pixel differencing on two FITS files
+"""
+def pixel_difference(image1_filename, image2_filename):
+
+    image1_file = os.path.join(ALIGNED_PATH, image1_filename)
+    image2_file = os.path.join(ALIGNED_PATH, image2_filename)
+
+    # Load the aligned FITS files
+    try:
+        with fits.open(image1_file) as hdul1, fits.open(image2_file) as hdul2:
+            data1 = hdul1[0].data
+            data2 = hdul2[0].data
+
+            # Resize images to a common size using interpolation
+            # Images are not all the same size. Not sure if this is a result of the cleaning algorithm or not.
+            min_shape = (min(data1.shape[0], data2.shape[0]), min(data1.shape[1], data2.shape[1]))
+            data1_resized = resize(data1, min_shape, mode='constant')
+            data2_resized = resize(data2, min_shape, mode='constant')
+
+            # Perform pixel-wise difference
+            diff_data = np.abs(data1_resized - data2_resized)  # Calculate absolute difference
+
+            # Save the difference image to a new FITS file
+            output_filename = DIFFERENCED_PATH + image2_filename
+            hdu = fits.PrimaryHDU(diff_data, header=hdul1[0].header)
+            hdul_out = fits.HDUList([hdu])
+            hdul_out.writeto(output_filename, overwrite=True)
+    except FileNotFoundError:
+        print(f"Could not find {image1_file} or {image2_file}. Previous steps may not have been completed.")
+        return
 
 """
 Call this to see a plot of the detected objects
@@ -159,26 +186,72 @@ def detect_objects_with_visualizer(filename):
     plt.show()
 
 
+"""
+helper for main 
+"""
+def get_random_files(directory, num_files):
+    files = [filename for filename in os.listdir(directory) if filename.lower().endswith('.fits')]
+    return random.sample(files, min(num_files, len(files)))
+
 
 if __name__ == "__main__":
-    if not os.path.exists(OUTPUT_PATH):
-        os.makedirs(OUTPUT_PATH)
 
+    fits_files = [filename for filename in os.listdir(INPUT_PATH) if filename.lower().endswith('.fits')]
+    # Delete prev test files with os.remove here if needed
+    if len(fits_files) < 2:
+        print("Not enough images to perform pixel differencing.")
+        exit()
+    
+    if not os.path.exists(ALIGNED_PATH):
+        os.makedirs(ALIGNED_PATH)
+    if not os.path.exists(GAUSSIANBLUR_PATH):
+        os.makedirs(GAUSSIANBLUR_PATH)
+    if not os.path.exists(DIFFERENCED_PATH):
+        os.makedirs(DIFFERENCED_PATH) 
+    if not os.path.exists(FLAGGED_PATH):
+        os.makedirs(FLAGGED_PATH) 
+
+    # Alignment
     base_source = None
-    for filename in os.listdir(INPUT_PATH):
-        # Check if the filename ends with '.fits' (case insensitive)
-        if filename.lower().endswith('.fits'):
-            if 'processed' in filename:  # Check if 'processed' is in the filename
-                os.remove(os.path.join(INPUT_PATH, filename))  # While testing - delete the file 
-            else:
-                if base_source is None:
-                    base_source = detect_objects(filename)
-                    subtract_background(filename, base_source)
-                else:
-                    cur_source = detect_objects(filename)
-                    subtract_background(filename, cur_source)
-                    align(base_source, cur_source, filename)
+    for filename in fits_files:
+        if base_source is None:
+            base_source = detect_objects(INPUT_PATH + filename)
+            subtract_background(filename, base_source)
+        else:
+            cur_source = detect_objects(INPUT_PATH + filename)
+            subtract_background(filename, cur_source)
+            align(base_source, cur_source, filename)
 
     if base_source is None:
         print("No FITS files found in the specified directory.")
         exit()
+
+    # Pixel Differencing
+    if len(fits_files) < 4:
+        fits_files = [filename for filename in os.listdir(INPUT_PATH) if filename.lower().endswith('.fits')]
+        # Iterate through each file and compare it to every other file
+        for i, base_filename in enumerate(fits_files):
+            for j, comparison_filename in enumerate(fits_files):
+                if i != j:  # Avoid comparing a file to itself
+                    base_path = os.path.join(INPUT_PATH, base_filename)
+                    comparison_path = os.path.join(INPUT_PATH, comparison_filename)
+                    pixel_difference(base_path, comparison_path)
+    else:            
+        for filename in os.listdir(INPUT_PATH):
+            if filename.lower().endswith('.fits'):                
+                # Get 3 random files for comparison
+                random_files = get_random_files(INPUT_PATH, 3)
+                
+                for random_filename in random_files:
+                    pixel_difference(filename, random_filename)
+
+    # Object detection on difference images
+    for filename in os.listdir(DIFFERENCED_PATH):
+        if filename.lower().endswith('.fits'):
+            sources = detect_objects(DIFFERENCED_PATH + filename)
+            # Move files with stars detected into flagged folder
+            if sources is None or len(sources) != 0:
+                os.rename(DIFFERENCED_PATH + filename, FLAGGED_PATH + filename)
+                print(f"Flagged {filename} for classification.")
+
+    
